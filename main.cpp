c1c78d6eb76d0100b6e8c851e0b6f7b5da3af546
Andy Xie <andyxie2013@hotmail.com>
2021-06-14 12:29:40 +1000

    new cpp testing along with python string cleaning

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 // my first program in C++
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
 
   int x;
-  std::cout << "Type a number: ";
-  std::cin >> x;
-  std::cout << "Your number is: " << x << "\n";
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Your number is: " << x << "\n";
+
+  string STRING;
+  ifstream hello("hello.txt");
+  //hello.open ("hello.txt", ios::trunc); 
+  while (getline (hello, STRING)) {
+  // Output the text from the file
+  cout << STRING << "\n";
+}  
+
+hello.close();
 }
---
+++ stopWords.py
@@ -0,0 +1,37 @@
+stopwords = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours	ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
+
+from nltk import *
+#nltk.download()
+
+from time import process_time
+
+t1_start = process_time()
+
+# load data
+filename = 'rawtext.txt'
+file = open(filename, 'rt')
+text = file.read()
+file.close()
+# split into words
+from nltk.tokenize import word_tokenize
+tokens = word_tokenize(text)
+# convert to lower case
+tokens = [w.lower() for w in tokens]
+# remove punctuation from each word
+import string
+table = str.maketrans('', '', string.punctuation)
+stripped = [w.translate(table) for w in tokens]
+# remove remaining tokens that are not alphabetic
+words = [word for word in stripped if word.isalpha()]
+# filter out stop words
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+#words = [w for w in words if not w in stop_words]
+words = [w for w in words]
+#print(words[:100])
+
+t1_stop = process_time()
+
+print("Elapsed time:", t1_stop, t1_start) 
+   
+print("Elapsed time during the whole program in seconds:", t1_stop-t1_start)
